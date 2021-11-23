ec978f7578a21fd43deef2b700dd0a50d63cd5ad
Jimmy Tang <jimmy.tang.360@gmail.com>
2021-11-23 15:19:57 +0000

    Head function that returns the first element in the array itself

---
+++ head.js
@@ -0,0 +1,20 @@
+// FUNCTION IMPLEMENTATION
+const head = function(firstItem) {
+  return firstItem[0];
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
