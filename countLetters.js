7151650729dd9930131c3c1c7c5aebb0c57a0cfc
Jimmy Tang <jimmy.tang.360@gmail.com>
2021-11-24 22:57:09 +0000

    Function that takes in a string and counts how many of each letter there are

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+/*
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1); */
+
+
+const countLetters = function(text) {
+const count = {};
+for (let i = 0; i < text.length; i++) {
+  count[text.charAt(i)] = text.split(text.charAt(i)).length - 1 
+}
+return count
+}
+console.log(countLetters("ABBCCCDDDDEEEEE"));
