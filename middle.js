8d6b8d3822d05550b2b439161d513415b2a1d434
Jimmy Tang <jimmy.tang.360@gmail.com>
2021-11-24 18:52:49 +0000

    Changed the testing lines to include assertArrayEquals

--- middle.js
+++ middle.js
@@ -39,10 +39,13 @@ const middle = function(array) {
   }
 };
 
-console.log(middle([1])); // => []
+/*console.log(middle([1])); // => []
 console.log(middle([1, 2])); // => []
 console.log(middle([1, 2, 3])); // => [2]
 console.log(middle([1, 2, 3, 4, 5])); // => [3]
 console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4] */
+
+console.log(middle([1, 2, 3, 4, 5]));
+assertArrayEquals(middle([1, 2, 3, 4, 5]), [3]);
 
