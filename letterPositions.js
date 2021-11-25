1d72d5c4019709c2cbbff108722ec434d4b19a5b
Jimmy Tang <jimmy.tang.360@gmail.com>
2021-11-25 01:19:47 +0000

    Function that counts the position of each character in the sentence and outputs the element it occurs in for each letter

---
+++ letterPositions.js
@@ -0,0 +1,44 @@
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo) === true ) {
+    console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+};
+
+//Equal Array Function
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//Function that checks and captures the position of each character in the given sentence
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    //If statement so it skips the spaces in the code
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [];
+        results[sentence[i]].push(i);
+      }
+    }
+  }
+  return results;
+};
+//Test
+console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
