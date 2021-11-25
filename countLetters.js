a3a0e627df1c83355daca8905497e0e21a398318
Jimmy Tang <jimmy.tang.360@gmail.com>
2021-11-25 00:20:29 +0000

    Modified the program to not count spaces when doing letter counts of the input

--- countLetters.js
+++ countLetters.js
@@ -16,8 +16,9 @@ assertEqual(1, 1); */
 const countLetters = function(text) {
 const count = {};
 for (let i = 0; i < text.length; i++) {
+  text = text.replace(/ /g, '');
   count[text.charAt(i)] = text.split(text.charAt(i)).length - 1 
 }
 return count
 }
-console.log(countLetters("ABBCCCDDDDEEEEE"));
+console.log(countLetters("ABBCCCDDDD EEEEE"));
