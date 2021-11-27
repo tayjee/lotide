f491c3be64d548f840a6d688071a0b05fc4e800b
Jimmy Tang <jimmy.tang.360@gmail.com>
2021-11-27 08:24:37 +0000

    Function that lets you find the key pertaining to the value inputted

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const findKeyByValue = function(object, value) {
+  const arrayKeys = Object.keys(object);
+  for (const key of arrayKeys) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+//TEST
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
