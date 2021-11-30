d820ff64088266a01b9d28abdf80594a17e899e7
Jimmy Tang <jimmy.tang.360@gmail.com>
2021-11-30 03:10:25 +0000

    Implemented chai to assert the test inputs

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,8 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 //TEST
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middle", () => {
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
